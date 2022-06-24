327c589f60594f877f667fe0d7c5625c5724178b
vitasiempre <wavelolmu@gmail.com>
2022-06-24 11:09:26 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,58 +1,13 @@
-// document.addEventListener("DOMContentLoaded", loadScript);
-//
-// let love;
-// let hate;
-//
-// async function loadScript() {
-//   let vesselApi = await fetch(url);
-//   vesselCall = await vesselApi.json();
-//   console.log(vesselCall);
-//
-//   let vesselTemplate = document.querySelector("#vessel").content;
-//   let vessel = vesselTemplate.cloneNode(true);
-//
-//   vessel.querySelector(".name").textContent = vesselCall["DATA"][0]["NAME"];
-//   vessel.querySelector(".mmsi").textContent = vesselCall["DATA"][0]["MMSI"];
-//   vessel.querySelector(".imo").textContent = vesselCall["DATA"][0]["IMO"];
-//   vessel.querySelector(".type").textContent =
-//     vesselCall["DATA"][0]["VESSEL_TYPE"];
-//   vessel.querySelector(".built").textContent = vesselCall["DATA"][0]["BUILD"];
-//
-//   document.querySelector(".vessel").appendChild(vessel);
-// }
-
-
-
-// var sync_object = {
-//   v_mycount: 0,
-//   getmycount() {
-//     return this.v_mycount;
-//   },
-//   setmycount(value) {
-//     if (this.v_mycount !== value) {
-//       this.v_mycount = value;
-//       this.update_dom();
-//     } else {
-//       console.log("Don't have to update doms when assign the same value");
-//     }
-//   },
-//  update_dom() {
-//    console.log('Update all doms which class name has "counter"');
-//    //if you use jquery you can update dom by this way
-//    $(".counter").contents(this.v_mycount);
-// }
-// };
-
 // import {hateTweets, hateTweets} from "/stream.js"
 
-let loveText = `25%LOVE
-25TWEETS`
-let hateText = `25%HATE
-25TWEETS`
+let loveText = `50%LOVE
+1400TWEETS`
+let hateText = `50%HATE
+1400TWEETS`
 
 var intFrameWidth = window.innerWidth;
 var intFrameHeight = window.innerHeight;
-var loveWidth = 500;
+var loveWidth = 720;
 var hateWidth = intFrameWidth - loveWidth - 20;
 var wordHeight = intFrameHeight * 0.41
 var marTop = (intFrameHeight - (wordHeight * 2 + 10)) / 2
@@ -73,7 +28,7 @@ function setup() {
   textFont(panama);
   textLeading(percentLeading);
   textSize(percentSize);
-  fill(148);
+  fill(107,108,111);
   text(loveText, 10, 10,500, 500);
   textAlign(RIGHT);
   text(hateText, intFrameWidth - 510, 10,500, 500);
@@ -93,13 +48,4 @@ function draw() {
   image(loveImg, 10, marTop, loveWidth, wordHeight);
   image(hateImg, loveWidth + 20, marTop, hateWidth - 10, wordHeight);
   image(relationshipImg, 10, wordHeight + 10 + marTop,intFrameWidth - 10,wordHeight);
-  // let lovepic = createImg('images/love.png', 'the p5 magenta asterisk');
-  // lovepic.class('lovePic');
-  // lovepic.position(0,0);
-
-  // let loveP = createP(loveText);
-  // loveP.style('font-family', 'panama');
-  // loveP.position(10, 0);
-
-
 }
--- src/stream.js
+++ src/stream.js
@@ -1,15 +1,10 @@
 import { ETwitterStreamEvent, TweetStream, TwitterApi, ETwitterApiError } from 'twitter-api-v2'
-import * as http from 'http';
-import * as path from 'path';
 
 const client = new TwitterApi( "AAAAAAAAAAAAAAAAAAAAAHFIeAEAAAAAyxZHe8mVWe6vBrf6LyjCu6BrpEw%3DN7J3OacND4q9bsMxFzGV2TMwmcCm2wZUbTtzhCySFqoR2xU3ZR"
 );
 
-const clientHate = new TwitterApi( "AAAAAAAAAAAAAAAAAAAAAHFIeAEAAAAAyxZHe8mVWe6vBrf6LyjCu6BrpEw%3DN7J3OacND4q9bsMxFzGV2TMwmcCm2wZUbTtzhCySFqoR2xU3ZR"
-);
-
-var love = 1;
-var hate = 1;
+var loveTweets = 1;
+var hateTweets = 1;
 
 const rules = await client.v2.streamRules();
 if (rules.data?.length) {
@@ -18,58 +13,32 @@ if (rules.data?.length) {
   });
 }
 
-// Add our rules
 await client.v2.updateStreamRules({
   add: [{ value: 'i love you' }, { value: 'i hate you' },],
 });
 
 const stream = await client.v2.searchStream();
-// Enable auto reconnect
 stream.autoReconnect = true;
 
-// streamLove.on(
-//   // Emitted when a Twitter payload (a tweet or not, given the endpoint).
-//   ETwitterStreamEvent.Data,
-//   eventData => {
-//     // love = love + 1;
-//     // console.log(eventData);
-//     eventData = trim(eventData);
-//     tweet = JSON.parse(eventData);
-//     console.log(tweet.data.text);
-//
-//   }
-// );
+stream.on(
+  ETwitterStreamEvent.Data,
+  eventData => {
+    tweet = JSON.parse(eventData);
+    console.log(tweet);
+    if (tweet.includes('love')) {
+      loveTweets = loveTweets + 1;
+    } else {
+      hateTweets = hateTweets + 1;
+    }
+  }
+);
 
-const loveTweets = await client.v2.tweetCountRecent('i love you');
-console.log(loveTweets.data[0].tweet_count);
+export { loveTweets, hateTweets }
 
-const hateTweets = await client.v2.tweetCountRecent('i hate you');
-console.log(hateTweets.data[0].tweet_count);
-
-export {hateTweets, hateTweets}
-//------------------------------------------------------
-
-// const rulesHate = await clientHate.v2.streamRules();
-// if (rulesHate.data?.length) {
-//   await clientHate.v2.updateStreamRules({
-//     delete: { ids: rulesHate.data.map(rule => rule.id) },
-//   });
-// }
+// const loveTweets = await client.v2.tweetCountRecent('i love you');
+// console.log(loveTweets.data[0].tweet_count);
 //
-// // Add our rules
-// await clientHate.v2.updateStreamRules({
-//   add: [{ value: 'i love you' }],
-// });
+// const hateTweets = await client.v2.tweetCountRecent('i hate you');
+// console.log(hateTweets.data[0].tweet_count);
 //
-// const streamHate = await clientHate.v2.searchStream();
-// // Enable auto reconnect
-// streamHate.autoReconnect = true;
-//
-// streamHate.on(
-//   // Emitted when a Twitter payload (a tweet or not, given the endpoint).
-//   ETwitterStreamEvent.Data,
-//   eventData => {
-//     hate = hate + 1
-//     console.log("hate", hate);
-//   }
-// );
+// export {hateTweets, loveTweets}
